caa742ab8210bfb652a3835a699e84695c8faf08
IamTechknow <techhexium@gmail.com>
2015-07-12 20:11:03 -0400

    Using newer plugin version, overhauled the program by replacing the surface view with a texture view.

--- asg2/src/main/java/com/IamTechknow/asg2/app/MainActivity.java
+++ asg2/src/main/java/com/IamTechknow/asg2/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.IamTechknow.asg2.app;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
-import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
@@ -11,6 +11,7 @@ import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraMetadata;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -21,12 +22,10 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.Surface;
-import android.view.SurfaceView;
-import android.view.SurfaceHolder;
+import android.view.TextureView;
 import android.util.Size;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 /**
  * Android Implementation of CMPE 161 Assignment 2. Uses the new Camera2 API found in API level 21 and above.
@@ -35,7 +34,6 @@ import java.util.List;
  * https://github.com/googlesamples/android-Camera2Basic/blob/master/Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
  * http://www.willowtreeapps.com/blog/camera2-and-you-leveraging-android-lollipops-new-camera/
  * http://pierrchen.blogspot.com/2015/01/android-camera2-api-explained.html
- * http://jylee-world.blogspot.com/2014/12/a-tutorial-of-androidhardwarecamera2.html
  */
 
 public class MainActivity extends Activity {
@@ -59,7 +57,7 @@ public class MainActivity extends Activity {
 
     /**
      * The program flow of control is: <br />
-     * 1) Add the surfaceview to the top-level view and attach the callback <br />
+     * 1) Add the texture view to the top-level view and attach the callback <br />
      * 2) In the callback, when the surface is created, do the following: <br />
      *      Setup the CameraManager
      *      Obtain the CameraCharacteristics for the back facing camera
@@ -67,17 +65,17 @@ public class MainActivity extends Activity {
      *      Set the size for the surfaceHolder
      *      open the CameraDevice, and set the CaptureSessionListener callback to start the capture session
      *
-     *    If the surfaceView changed (due to rotation or on app resume), set the biggest size again
+     *    If the texture view changed (due to rotation or on app resume), set the biggest size again
      *
-     *    If the surfaceView is destroyed, end the capture session <br />
+     *    If the texture view is destroyed, end the capture session <br />
      *
      * 3) In the CameraDevice callback:
-     *      Put the surfaceholder's surface into a list
+     *      Put the texture surface into a list
      *      Setup a camera preview request and make it repeat
      *
      * 4) In the CaptureSession state callback:
      *      Create a request for a camera preview
-     *      Add a output surface for the request
+     *      Add the texture surface for the request
      *      Build the request, and set it to be repeating
      */
     public static class PlaceholderFragment extends Fragment {
@@ -89,10 +87,10 @@ public class MainActivity extends Activity {
         private String mCameraId;
         private CameraCaptureSession mCaptureSession;
         private Size mPreviewSize;
+        private CaptureRequest.Builder mPreviewBuilder;
 
         //Surface fields
-        private SurfaceView mSurfaceView;
-        private SurfaceHolder mSurfaceHolder;
+        private TextureView mTextureView;
 
         //Thread fields
         private Handler mPreviewHandler;
@@ -105,12 +103,27 @@ public class MainActivity extends Activity {
                 //When the camera is opened start the preview
                 Log.i(TAG, "Successfully opened camera");
                 mCamera = camera;
+                SurfaceTexture texture = mTextureView.getSurfaceTexture();
+                if (texture == null) {
+                    Log.e(TAG, "texture is null");
+                    return;
+                }
+
+                texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+                Surface surface = new Surface(texture);
+
                 try {
-                    List<Surface> outputs = Arrays.asList( //capture session needs a list of surfaces
-                            mSurfaceView.getHolder().getSurface());
-                    mCamera.createCaptureSession(outputs, mPreviewStateCallback, mPreviewHandler);
+                    mPreviewBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                } catch (CameraAccessException e){
+                    e.printStackTrace();
+                }
+
+                mPreviewBuilder.addTarget(surface);
+                try { //Create a list containing the surface before creating session
+                    mCamera.createCaptureSession(Collections.singletonList(surface), mPreviewStateCallback, null);
                 } catch (CameraAccessException e) {
                     Log.e(TAG, "Failed to create a capture session", e);
+                    e.printStackTrace();
                 }
 
             }
@@ -126,49 +139,44 @@ public class MainActivity extends Activity {
             }
         };
 
-        private SurfaceHolder.Callback mSurfaceCallback = new SurfaceHolder.Callback() {
+        private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+
             @Override
-            public void surfaceCreated(SurfaceHolder holder) { //Setup the camera here
-                Log.i(TAG, "Surface created");
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+                //Log.i(TAG, "onSurfaceTextureUpdated()");
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+                Log.i(TAG, "onSurfaceTextureSizeChanged()");
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                Log.i(TAG, "onSurfaceTextureDestroyed()");
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                Log.i(TAG, "onSurfaceTextureAvailable()");
+
                 mCameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
-                try { //Get information about the camera to create the CameraDevice
+                try{ //Get information about the camera to create the CameraDevice
                     mCameraId = mCameraManager.getCameraIdList()[0];
                     mCameraCharacteristics = mCameraManager.getCameraCharacteristics(mCameraId);
 
                     //Get supported file formats and sizes and get the largest preview size
                     StreamConfigurationMap map = mCameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                    mPreviewSize = getBestSize(map.getOutputSizes(ImageFormat.JPEG));
-                    mSurfaceHolder.setFixedSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+                    mPreviewSize = getBestSize(map.getOutputSizes(SurfaceTexture.class));
 
-                    mCameraManager.openCamera(mCameraId, mStateCallback, mPreviewHandler); //open camera. null = use main thread looper
-                } catch (CameraAccessException e) {
+                    mCameraManager.openCamera(mCameraId, mStateCallback, null);
+                }
+                catch(CameraAccessException e) {
                     Log.e(TAG, "Unable to list cameras", e);
                     e.printStackTrace();
                 }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                //surface Changed, set new preview size
-                StreamConfigurationMap map = mCameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                mPreviewSize = getBestSize(map.getOutputSizes(ImageFormat.JPEG));
-                mSurfaceHolder.setFixedSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                Log.i(TAG, "Surface destroyed");
-                //Close the camera session and camera
-                if(mCaptureSession != null) {
-                    mCaptureSession.close();
-                    mCaptureSession = null;
-                }
-
-                if (mCamera != null) {
-                    mCamera.close();
-                    mCamera = null;
-                }
-
             }
         };
 
@@ -177,22 +185,16 @@ public class MainActivity extends Activity {
             public void onConfigured(CameraCaptureSession session) {
                 //Request for preview footage and set a repeating request
                 mCaptureSession = session;
-                if(mSurfaceHolder != null) {
-                    try { //Create a capture request and adds the surfaceview's surface as the target
-                        CaptureRequest.Builder requestBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                        requestBuilder.addTarget(mSurfaceHolder.getSurface());
-                        CaptureRequest previewRequest = requestBuilder.build();
 
-                        try {
-                            mCaptureSession.setRepeatingRequest(previewRequest, null, null); //no metadata, use main thread
-                        } catch (CameraAccessException e) {
-                            Log.e(TAG, "Failed to make repeating preview request", e);
-                        }
-                    } catch (CameraAccessException e) {
-                        Log.e(TAG, "Failed to build preview request", e);
-                    }
-                } else
-                    Log.e(TAG, "Holder didn't exist when trying to formulate preview request");
+                mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+                setupHandler();
+
+                try {
+                    mCaptureSession.setRepeatingRequest(mPreviewBuilder.build(), null, mPreviewHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -224,12 +226,16 @@ public class MainActivity extends Activity {
             setupHandler();
             mCameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
 
-            //Resume camera session
-            try {
-                mCameraManager.openCamera(mCameraId, mStateCallback, mPreviewHandler);
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
-            }
+            if(mCameraId != null)
+                //Resume camera session
+                try {
+                    if(mTextureView.isAvailable())
+                        mCameraManager.openCamera(mCameraId, mStateCallback, mPreviewHandler);
+                    else
+                        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
         }
 
         @Override
@@ -252,11 +258,9 @@ public class MainActivity extends Activity {
                                  Bundle savedInstanceState) {
             View v = inflater.inflate(R.layout.fragment_main, container, false);
 
-            //Obtain the surface holder to implement callbacks for displaying raw pixel data
-            mSurfaceView = (SurfaceView) v.findViewById(R.id.camera_view);
-            mSurfaceHolder = mSurfaceView.getHolder();
-
-            mSurfaceHolder.addCallback(mSurfaceCallback);
+            //Obtain the texture view to implement callbacks for displaying raw pixel data
+            mTextureView = (TextureView) v.findViewById(R.id.camera_view);
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
 
             return v;
         }
